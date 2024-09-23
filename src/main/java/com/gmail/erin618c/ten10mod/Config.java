1552abf9d0c392dc6fadfcadd3a408b137cf5eee
erincocker <erin618c@gmail.com>
2024-09-23 12:30:00 +0100

    package names changed and a setting in the toml file

--- src/main/java/com/gmail/erin618c/ten10mod/Config.java
+++ src/main/java/com/gmail/erin618c/ten10mod/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.gmail.erin618c.ten10mod;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
--- src/main/java/com/gmail/erin618c/ten10mod/ExampleMod.java
+++ src/main/java/com/gmail/erin618c/ten10mod/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.gmail.erin618c.ten10mod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
