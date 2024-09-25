9979e312c8b22ec088b28a8565c4fd6ecdf05e5a
erincocker <erin618c@gmail.com>
2024-09-25 09:27:24 +0100

    removed unused imports

--- src/main/java/com/gmail/erin618c/ten10mod/item/ModItems.java
+++ src/main/java/com/gmail/erin618c/ten10mod/item/ModItems.java
@@ -1,8 +1,5 @@
 package com.gmail.erin618c.ten10mod.item;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
